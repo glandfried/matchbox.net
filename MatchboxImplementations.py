009d0d74383ddfafee56ac09588a7e4d349c75db
makobot-sh <45239258+makobot-sh@users.noreply.github.com>
2023-01-09 21:12:20 -0300

    commit to change pc

--- MatchboxImplementations.py
+++ MatchboxImplementations.py
@@ -1,32 +1,34 @@
 import InfernetWrapper
 from InfernetWrapper import *
+from surprise import SVDpp
 
-def matchbox():
+class Rating():
+    def __init__(self, user, item, value):
+        self.user = user
+        self.item = item
+        self.value = value #can be none
+
+def infer_dotnet(datasetName, rating, traitCount=5, iterationCount=20):
     # Ejemplo de https://dotnet.github.io/infer/userguide/Learners/Matchbox%20recommender/Learner%20API.html
     dataMapping = CsvMapping()
     recommender = MatchboxCsvWrapper.Create(dataMapping)
     # Settings: https://dotnet.github.io/infer/userguide/Learners/Matchbox/API/Setting%20up%20a%20recommender.html
-    recommender.Settings.Training.TraitCount = 5;
-    recommender.Settings.Training.IterationCount = 20;
-    recommender.Train("./data/MovieLens/data_50.csv");
+    recommender.Settings.Training.TraitCount = traitCount;
+    recommender.Settings.Training.IterationCount = iterationCount;
+    recommender.Train(datasetName);
 
-    '''
-    recommender.Save("TrainedModel.bin");
-    // ...
-    recommender = MatchboxRecommender.Load<string, string, string, NoFeatureSource>( "TrainedModel.bin");
-    '''
     # Modos prediccion: https://dotnet.github.io/infer/userguide/Learners/Matchbox/API/Prediction.html
-    posterior = recommender.PredictDistribution("196","302");
-    print("Posterior rating para usuario 196 e item 302 con 5 estrellas");
-    print(",".join(map(str,posterior)));
-    recommendations = recommender.Recommend("196", 10);
-    print(",".join(map(str,recommendations)));
-
+    #print("Posterior rating para usuario 196 e item 302 con 5 estrellas");
+    #posterior = recommender.PredictDistribution(rating.user, rating.item);
+    #print(",".join(map(str,posterior)));
+    recommendations = recommender.Recommend(rating.user, 10);
+    #print(",".join(map(str,recommendations)));
+    return recommendations
+    '''
     print("Imple dotnet:")
     dotnet_posteriors = {0: 0.12841634145715716, 1: 0.14323634733764679, 2: 0.16840827972422545, 3: 0.2646673070766022, 4: 0.10161728482043446, 5: 0.19365443958393389}
     print(dotnet_posteriors);
     print("Diferencia con implementacion en dotnet:")
     dotnet_ranking= [242,327,234,603,1014,387,95,222,465,201]
     print(["{0:.20f}".format(abs(dotnet_posteriors[i]-posterior[i])) for i in range(len(posterior))])
-
-matchbox()
+    '''
---
+++ main.py
@@ -0,0 +1,4 @@
+import MatchboxImplementations
+
+rating = MatchboxImplementations.Rating("196","302",None)
+MatchboxImplementations.infer_dotnet("./data/MovieLens/data_50.csv")
