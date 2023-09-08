446ad2adbd2c102a9e61255b6bd7959aaedaa363
makobot-sh <45239258+makobot-sh@users.noreply.github.com>
2023-09-08 14:31:46 -0300

    Plot user and item traits

--- Simulation.py
+++ Simulation.py
@@ -15,7 +15,7 @@ def generateItems(numItems, numTraits, a=0.5, b=0.5):
 
     #plt.plot(grilla, beta.pdf(grilla, a, b), 'r-', lw=5, alpha=0.6, label='beta pdf')
     #plt.savefig("./tmp/beta.png")
-    #plt.clf()
+    #plt.close()
 
     res = []
     for i in range(100):
@@ -29,7 +29,7 @@ def plotGaussian(mean, var, name):
     grilla = list(np.arange(0,1,0.01))
     plt.plot(grilla, norm.pdf(grilla, mean, var), '-', title=f'{name} - mean:{mean}, var:{var}')
     plt.savefig(f"./tmp/{name}.png")
-    plt.clf()
+    plt.close()
 
 def plotThresholds(gauss, user):
     grilla = list(np.arange(-5,5,0.1))
@@ -48,11 +48,38 @@ def plotThresholds(gauss, user):
     plt.title(f"User {user} thresholds")
     plt.legend()
     plt.savefig( f"./tmp/user{user}_thresholds.png")
-    plt.clf()
+    plt.close()
+
+def plotItemTraits(gauss, item, isUser=False):
+    N = 100
+
+    # Create subplots 
+    fig, axes = plt.subplots(nrows=3, ncols=2)
+    fig.subplots_adjust(hspace=1)
+    fig.suptitle(f'Traits of movie {item}')
+    fig.set_figheight(8)
+    fig.set_figwidth(7)
+
+    # Generate dist plots
+    grilla = list(np.arange(-1,1,0.01))
+    for i, ax, g in zip(range(len(gauss)), axes.flatten(), gauss):
+        mean = g.GetMean()
+        var = g.GetVariance()
+        ax.plot(grilla, norm.pdf(grilla, mean, var), '-', label=f'{i}: ({mean:.1f}, {var:.1f})')
+        ax.legend()
+        ax.set_title(f"Trait {i}")
+    
+    plt.savefig( f"./tmp/{'item' if not isUser else 'user'}{item}_traits.png")
+    plt.close()
 
 class UserThresholds():
     balancedRating = [(-np.inf, 0.0), (-2.55, 0.32), (-1.16, 0.24), (0.0, 0.0), (1.19, 0.25), (2.69, 0.39), (np.inf, 0.0)]
-    
+
+def Simulation():
+    movies = generateItems(100, 5)
+    people = generateItems(10, 5)
+
+
 if __name__ == "__main__":
     movies = generateItems(100, 5)
     people = generateItems(10, 5)
@@ -66,19 +93,20 @@ if __name__ == "__main__":
         ttsi = TrainTestSplitInstance(dataset)
         ttsi.loadDatasets(preprocessed=True, NROWS=None, BATCH_SIZE=None)
         mbox=Matchbox(ttsi, max_trials=1)
-        recommender = mbox.createRecommender(mbox.bestParams())
+        params = mbox.bestParams()
+        params["traitCount"] = 5
+        recommender = mbox.createRecommender(params)
         _ = mbox.train(recommender)
 
         userPosteriors = recommender.GetPosteriorDistributions().Users
         itemPosteriors = recommender.GetPosteriorDistributions().Items
-
-        """
+        
         for user in userPosteriors.Keys:
             posteriors = userPosteriors.get_Item(user)
             #os.makedirs(f"./tmp/user{user}", exist_ok=True)
             plotThresholds(posteriors.Thresholds, user)
-            print([(float("{:.2f}".format(g.GetMean())), float("{:.2f}".format(g.GetVariance()))) for g in posteriors.Thresholds])
-        """
+            plotItemTraits(posteriors.Traits, user, isUser=True)
+            #print([(float("{:.2f}".format(g.GetMean())), float("{:.2f}".format(g.GetVariance()))) for g in posteriors.Thresholds])  
 
         for item in itemPosteriors.Keys:
             posteriors = itemPosteriors.get_Item(item)
@@ -86,6 +114,7 @@ if __name__ == "__main__":
             #plotThresholds(posteriors.Thresholds, item)
             print(f"ITEM {item}")
             print([(float("{:.2f}".format(g.GetMean())), float("{:.2f}".format(g.GetVariance()))) for g in posteriors.Traits])
+            plotItemTraits(posteriors.Traits, item)
 
         input("Press Enter to continue...") 
 
--- data/Tests/generate_test_data.py
+++ data/Tests/generate_test_data.py
@@ -4,48 +4,65 @@ import os
 MAX_RATING = 5
 MIN_RATING = 0
 
-def rate(file, users, movies, rating):
+def rate(file, users, movies, rating, ts=999):
   for u in users:
     for m in movies:
-      file.write(f"{u},{m},{rating},999\n")
+      file.write(f"{u},{m},{rating},{ts}\n")
 
 def test1(folder="data/Tests/test1"):
   os.makedirs(folder, exist_ok=True)
   with open(f"{folder}/ratings_train.csv", "w") as file:
     rate(file, [0], [1,2,3,4,5], MIN_RATING)
-    rate(file, file, [1,2,3,4,5], [0], MAX_RATING)
+    rate(file, [1,2,3,4,5], [0], MAX_RATING)
   with open(f"{folder}/ratings_test.csv", "w") as file:
-    rate(file, [0], [0], MAX_RATING)
+    rate(file, [0], [0], MAX_RATING, 1000)
 
 def test2(folder="data/Tests/test2"):
   os.makedirs(folder, exist_ok=True)
   test1(folder)
-  with open(f"{folder}/ratings_train.csv", "w+") as file:
+  with open(f"{folder}/ratings_train.csv", "a") as file:
     rate(file, [1], [1,2,3,4,5], MIN_RATING)
 
 def test3(folder="data/Tests/test3"):
   os.makedirs(folder, exist_ok=True)
   with open(f"{folder}/ratings_train.csv", "w") as file:
     rate(file, [0], [1,2,3,4,5], MAX_RATING)
-    rate(file, file, [1,2,3,4,5], [0], MAX_RATING)
+    rate(file, [1,2,3,4,5], [0], MAX_RATING)
   with open(f"{folder}/ratings_test.csv", "w") as file:
-    rate(file, [0], [0], MAX_RATING)
+    rate(file, [0], [0], MAX_RATING, 1000)
 
-def test_med(folder, med_rating):
+def test_med(med_rating, folder):
   with open(f"{folder}/ratings_train.csv", "w") as file:
     rate(file, [0], [1,2,3,4,5], med_rating)
     rate(file, [1], [1,2,3,4,5], med_rating)
-    rate(file, file, [1,2,3,4,5], [0], MAX_RATING)
+    rate(file, [1,2,3,4,5], [0], MAX_RATING)
   with open(f"{folder}/ratings_test.csv", "w") as file:
-    rate(file, [0], [0], MAX_RATING)
+    rate(file, [0], [0], MAX_RATING, 1000)
 
 def test4(folder="data/Tests/test4"):
   os.makedirs(folder, exist_ok=True)
-  test_med(3)
+  test_med(3, folder)
 
 def test5(folder="data/Tests/test5"):
   os.makedirs(folder, exist_ok=True)
-  test_med(2)
+  test_med(2, folder)
+
+def test6(folder="data/Tests/test6"):
+  os.makedirs(folder, exist_ok=True)
+  test_med(2, folder)
+  with open(f"{folder}/ratings_train.csv", "a") as file:
+    rate(file, [1,2,3,4,5], [7,8,9], MAX_RATING)
+
+def test7(folder="data/Tests/test7"):
+  with open(f"{folder}/ratings_train.csv", "w") as file:
+    rate(file, list(range(0,6)), [0], 0)
+    rate(file, list(range(0,6)), [1], 1)
+    rate(file, list(range(0,6)), [2], 2)
+    rate(file, list(range(0,6)), [3], 3)
+    rate(file, list(range(0,6)), [4], 4)
+    rate(file, list(range(0,6)), [5], 5)
+  with open(f"{folder}/ratings_test.csv", "w") as file:
+    rate(file, [0], [0], MAX_RATING, 1000)
 
 def test8(folder="data/Tests/test8"):
   os.makedirs(folder, exist_ok=True)
@@ -57,7 +74,25 @@ def test8(folder="data/Tests/test8"):
     rate(file, list(range(0,6)), [4,10,16], 4)
     rate(file, list(range(0,6)), [5,11,17], 5)
   with open(f"{folder}/ratings_test.csv", "w") as file:
-    rate(file, [0], [0], MAX_RATING)
+    rate(file, [0], [0], MAX_RATING, 1000)
+
+def test9(folder="data/Tests/test9"):
+  os.makedirs(folder, exist_ok=True)
+  with open(f"{folder}/ratings_train.csv", "w") as file:
+    for i in range(0,6):
+      rate(file, [i], [i], MAX_RATING)
+      if (i-1) >= 0: rate(file, [i], [i-1], MAX_RATING-1)
+      if (i+1) <= 5: rate(file, [i], [i+1], MAX_RATING-1)
+      if (i-2) >= 0: rate(file, [i], [i-2], MAX_RATING-2)
+      if (i+2) <= 5: rate(file, [i], [i+2], MAX_RATING-2)
+      if (i-3) >= 0: rate(file, [i], [i-3], MAX_RATING-3)
+      if (i+3) <= 5: rate(file, [i], [i+3], MAX_RATING-3)
+      if (i-4) >= 0: rate(file, [i], [i-4], MAX_RATING-4)
+      if (i+4) <= 5: rate(file, [i], [i+4], MAX_RATING-4)
+      if (i-5) >= 0: rate(file, [i], [i-5], MAX_RATING-5)
+      if (i+5) <= 5: rate(file, [i], [i+5], MAX_RATING-5)
+  with open(f"{folder}/ratings_test.csv", "w") as file:
+    rate(file, [0], [0], MAX_RATING, 1000)
 
 def generar_tests_csvs():
   test1()
@@ -65,6 +100,9 @@ def generar_tests_csvs():
   test3()
   test4()
   test5()
-  
-test8()
+  test6()
+  test7()
+  test8()
+
+test9()
 #generar_tests_csvs()
