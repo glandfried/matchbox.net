15db8597833aade9ac381621277900923fe5c640
makobot-sh <45239258+makobot-sh@users.noreply.github.com>
2024-06-13 03:29:35 -0300

    Running exps for synthetic 100k datasets

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import pandas as pd
 import numpy as np
 
 
-dataset = "./data/MovieLens/ml-100k/ratings.csv"
-#dataset = "./data/Simulation/20240114_02-11-20_inferUpdate/ratings.csv"
+#dataset = "./data/MovieLens/ml-100k/ratings.csv"
+dataset = "./data/Simulation/tutorial_synth/100k/ratings.csv"
 ttsi = TrainTestSplitInstance(dataset)
 ttsi.loadDatasets(preprocessed=True, NROWS=None, BATCH_SIZE=None)
 print("Run experiments for 1 to 5 star 100k movielens....")
@@ -27,8 +27,8 @@ df_lgbm=lgbm.bestCandidates()
 print("LGBM", df_lgbm)
 
 print("Run experiments for binary 100k movielens....")
-dataset = "./data/MovieLens/ml-100k-binary/ratings.csv"
-#dataset = "./data/Simulation/20240114_02-11-20_inferUpdate/ratings.csv"
+#dataset = "./data/MovieLens/ml-100k-binary/ratings.csv"
+dataset = "./data/Simulation/tutorial_synth/100k-binary/ratings.csv"
 ttsi = TrainTestSplitInstance(dataset)
 ttsi.loadDatasets(preprocessed=True, NROWS=None, BATCH_SIZE=None)
 mbox=Matchbox(ttsi, max_trials=10)
